6f805b515bd0215cf5f21d0fa0b58e0df4f0775b
Dhruvi Ranwala <144709918+dhruvii-i@users.noreply.github.com>
2024-02-14 22:45:09 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-package Assignment3;
-
-public class Main {
-    public static void main(String[] args) {
-        StudentManager studentManager = new StudentManager();
-        studentManager.run();
-    }
-}
